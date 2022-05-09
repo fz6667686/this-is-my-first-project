06e19a91a179f4edff606b6900c232c15219863e
fz6667686 <105201137+fz6667686@users.noreply.github.com>
2022-05-09 11:34:27 +0300

    Add files via upload

---
+++ oc_bros.bat
@@ -0,0 +1,110 @@
+Echo off
+:main menu
+title oc_bros
+echo ##############
+echo ##############
+echo ####oc_bros####
+echo ##############
+echo ##############
+echo ###1)menu#####
+echo ###2)exit######
+echo ##############
+set /p var=set command
+if %var%==1 goto menu
+if %var%==2 goto exit
+:menu
+cls
+echo ##############
+echo ##############
+echo ####3)home####
+echo ###4)explorer##
+echo ##############
+echo ##############
+set /p var=set command
+if %var%==3 goto home
+if %var%==4 goto explorer
+:home
+cls
+Echo off
+:main menu
+title oc_bros
+echo ##############
+echo ##############
+echo ####oc_bros###
+echo ##############
+echo ##############
+echo ###1)menu#####
+echo ###2)exit######
+echo ##############
+set /p var=set command
+if %var%==1 goto menu
+if %var%==2 goto exit
+:menu
+cls
+echo ##############
+echo ##############
+echo ####3)home####
+echo ###4)explorer##
+echo ##############
+echo ##############
+set /p var=set command
+if %var%==3 goto home
+if %var%==4 goto explorer
+:explorer
+start explorer
+cls
+Echo off
+:main menu
+title oc_bros
+echo ##############
+echo ##############
+echo ####oc_bros####
+echo ##############
+echo ##############
+echo ###1)menu#####
+echo ###2)exit######
+echo ##############
+set /p var=set command
+if %var%==1 goto menu
+if %var%==2 goto exit
+:menu
+cls
+echo ##############
+echo ##############
+echo ####3)home####
+echo ###4)explorer##
+echo ##############
+echo ##############
+set /p var=set command
+if %var%==3 goto home
+if %var%==4 goto explorer
+:home
+cls
+Echo off
+:main menu
+title oc_bros
+echo ##############
+echo ##############
+echo ####oc_bros###
+echo ##############
+echo ##############
+echo ###1)menu#####
+echo ###2)exit######
+echo ##############
+set /p var=set command
+if %var%==1 goto menu
+if %var%==2 goto exit
+:menu
+cls
+echo ##############
+echo ##############
+echo ####3)home####
+echo ###4)explorer##
+echo ##############
+echo ##############
+set /p var=set command
+if %var%==3 goto home
+if %var%==4 goto explorer
+:explorer
+start explorer
+
